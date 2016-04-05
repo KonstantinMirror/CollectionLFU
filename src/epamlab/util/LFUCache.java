d2cbefd9c4b27d75f193f22af659cac2de497f7b
mirror <mironosk@gmail.com>
2016-04-05 23:21:03 +0300

    new class

--- src/epamlab/util/LFUCache.java
+++ src/epamlab/util/LFUCache.java
@@ -3,18 +3,19 @@ package epamlab.util;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class LFUCache<K, V> {
 
 	private final int CACHE_SIZE;
 	private final double EVICTION_FACTOR;
 
-	private Map<K, V> map;
-	private List<List<K>> listRow;
-	private List<K> row;
+	private Map<K, Pair<V>> map;
+	private List<Set<K>> listRow;
 
 	public LFUCache(int size, double evictionFactor) {
 		super();
@@ -23,74 +24,35 @@ public class LFUCache<K, V> {
 		map = new HashMap<>();
 		listRow = new ArrayList<>(CACHE_SIZE);
 		for (int i = CACHE_SIZE; i > 0; i--) {
-			listRow.add(new ArrayList<>(CACHE_SIZE));
-
+			listRow.add(new LinkedHashSet<>());
 		}
-		row = new ArrayList<>();
 	}
 
 	public void put(K k, V v) {
 		if (!map.containsKey(k)) {
-			map.put(k, v);
-			listRow.get(0).add(k);
+			listRow.get(0).add(k);			
+			map.put(k, new Pair<>(v, 0));
 		}
 	}
 
 	
-	
-	
-	
-	
-	
-	
-	
-	private class Pair {
-		private long row;
-		private long line;
 
-		public Pair() {
-		}
-
-		public Pair(long row, long line) {
-			super();
-			this.row = row;
-			this.line = line;
-		}
-
-		public long getRow() {
-			return row;
-		}
-
-		public void setRow(long row) {
-			this.row = row;
-		}
-
-		public long getLine() {
-			return line;
-		}
-
-		public void setLine(long line) {
-			this.line = line;
-		}
-
-	}
-
-	public K get(K t) {
-		if (map.containsKey(t)) {
-			int countGet = map.get(t);
-			map.put(t, countGet + 1);
+	public K get(K k) {
+		if (map.containsKey(k)) {
+			Pair<V> value = map.get(k);
+			
+			map.put(k, countGet + 1);
 			if (!(countGet > CACHE_SIZE)) {
-				listRow.get(countGet - 1).remove(t);
-				listRow.get(countGet).add(t);
+				listRow.get(countGet - 1).remove(k);
+				listRow.get(countGet).add(k);
 			}
 		}
-		return t;// ?????
+		return k;// ?????
 	}
 
 	private void evict() {
 		long evictElements = Math.round(EVICTION_FACTOR * CACHE_SIZE);
 		for (List<K> listDate : listRow) {
-
 			if (!listDate.isEmpty()) {
 				Iterator<K> iterator = listDate.iterator();
 				while (iterator.hasNext()) {
@@ -104,5 +66,40 @@ public class LFUCache<K, V> {
 				}
 			}
 		}
+	}
+	
+	
+	
+	
+	
+	private class Pair<T> {
+		private T t;
+		private long row;
+
+		public Pair() {
+		}
+
+		public Pair(T t, long row) {
+			super();
+			this.row = row;
+			this.t = t;
+		}
+
+		public T getK() {
+			return t;
+		}
+
+		public void setK(T t) {
+			this.t = t;
+		}
+
+		public long getRow() {
+			return row;
+		}
+
+		public void setRow(long row) {
+			this.row = row;
+		}
+
 	}
 }
