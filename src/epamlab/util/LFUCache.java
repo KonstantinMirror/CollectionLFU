8b55cb0e15305de75b2c47896ee5b4d5e795e0a9
mirror <mironosk@gmail.com>
2016-04-05 14:42:06 +0300

    some change

--- src/epamlab/util/LFUCache.java
+++ src/epamlab/util/LFUCache.java
@@ -2,19 +2,19 @@ package epamlab.util;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class LFUCache<T> {
 
 	private final int CACHE_SIZE;
 	private final double EVICTION_FACTOR;
+	private final int START_VALUE = 0;
 
 	private Map<T, Integer> map;
-	private List<List<Map<T, Integer>>> listRow;
-	List<Entry<T, Integer>> rowData;
-	List<Map<T, Integer>> row;
+	private List<List<T>> listRow;
+	private List<T> row;
 
 	public LFUCache(int size, double ecictionFactor) {
 		super();
@@ -22,16 +22,48 @@ public class LFUCache<T> {
 		EVICTION_FACTOR = ecictionFactor;
 		map = new HashMap<>();
 		listRow = new ArrayList<>();
-		rowData = new ArrayList<>();
+		for (int i = CACHE_SIZE; i > 0; i--) {
+			listRow.add(new ArrayList<>());
+
+		}
 		row = new ArrayList<>();
 	}
 
-	public void add(T t) {
+	public void put(T t) {
+
 		if (!map.containsKey(t)) {
-			
-
+			listRow.get(0).add(t);
 		}
-
 	}
 
+	public T get(T t) {
+		if (map.containsKey(t)) {
+			int countGet = map.get(t);
+			map.put(t, countGet + 1);
+			if (!(countGet > CACHE_SIZE)) {
+				listRow.get(countGet - 1).remove(t);
+				listRow.get(countGet).add(t);
+			}
+		}
+		return t;// ?????
+	}
+
+	private void evict() {
+		long evictElements = Math.round(EVICTION_FACTOR * CACHE_SIZE);
+		for (List<T> listDate : listRow ) {
+			
+			if(!listDate.isEmpty()){
+				Iterator<T> iterator = listDate.iterator();
+				while (iterator.hasNext()) {
+					T t = iterator.next();
+					map.remove(t);
+					iterator.remove();
+					evictElements--;
+					if(evictElements == 0){
+						
+					}
+				}
+ 			}
+		}
+	}
 }
